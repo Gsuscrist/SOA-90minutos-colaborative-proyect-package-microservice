9055e46c6cf5651b53d63a8b1ccb7dee16fcfa85
Mauricio David Castillo Ruvalcaba <211106@ids.upchiapas.edu.mx>
2024-06-19 21:07:45 -0600

    [add] Delete method for Package entity

--- src/package/infrastructure/dependencies.ts
+++ src/package/infrastructure/dependencies.ts
@@ -8,6 +8,9 @@ import {GetPackageController} from "./controllers/getPackageController";
 import { GetAllPackagesUseCase } from "../application/useCase/getAllPackageUseCase";
 import { GetAllPackagesController } from "./controllers/getAllPackagesController";
 
+import { DeletePackageUseCase } from "../application/useCase/deletePackageUseCase";
+import { DeletePackageController } from "./controllers/deletePackageController";
+
 const mysqlPackageRepository = new MysqlPackageRepository();
 
 const createPackageUseCase = new CreatePackageUseCase(mysqlPackageRepository);
@@ -19,3 +22,7 @@ export const getPackageController = new GetPackageController(getPackageUseCase);
 
 const getAllPackagesUseCase = new GetAllPackagesUseCase(mysqlPackageRepository);
 export const getAllPackagesController = new GetAllPackagesController(getAllPackagesUseCase);
+
+
+const deletePackageUseCase = new DeletePackageUseCase(mysqlPackageRepository);
+export const deletePackageController = new DeletePackageController(deletePackageUseCase);

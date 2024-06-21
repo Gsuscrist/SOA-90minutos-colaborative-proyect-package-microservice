6f60373881dd8023ec499b87c30042be678bd0ad
Gsuscrist <jago2811@gmail.com>
2024-06-20 20:46:39 -0600

    [fix] object return from repository

--- src/package/infrastructure/routes/packageRoutes.ts
+++ src/package/infrastructure/routes/packageRoutes.ts
@@ -7,7 +7,7 @@ packageRoutes.post('/', createPackageController.run.bind(createPackageController
 
 packageRoutes.get('/v1/get', getAllPackagesController.handle.bind(getAllPackagesController))
 
-packageRoutes.get('v1/get/:id', getPackageController.handle.bind(getPackageController))
+packageRoutes.get('/v1/get/:id', getPackageController.handle.bind(getPackageController))
 
 //! Uncomment
 // packageRoutes.delete('v1/get/:id', deletePackageController.handle.bind(deletePackageController));

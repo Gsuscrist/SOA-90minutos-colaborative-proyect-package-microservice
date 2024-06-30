5a7d9c4e872f4704d470fc39a5cc3a424d5b5791
Gsuscrist <jago2811@gmail.com>
2024-06-30 10:47:24 -0600

    [fix] endpoints

--- src/package/infrastructure/routes/packageRoutes.ts
+++ src/package/infrastructure/routes/packageRoutes.ts
@@ -11,10 +11,10 @@ import {
 
 export const packageRoutes = express.Router();
 
-packageRoutes.post('/', createPackageController.run.bind(createPackageController));
+packageRoutes.post('/v1/post', createPackageController.run.bind(createPackageController));
 packageRoutes.get('/v1/get', getAllPackagesController.handle.bind(getAllPackagesController))
 packageRoutes.get('/v1/get/:id', getPackageController.handle.bind(getPackageController))
 packageRoutes.put('/v1/put', updatePackageController.run.bind(updatePackageController));
-packageRoutes.patch('/v1/updateStatus', updateStatusController.run.bind(updateStatusController));
-packageRoutes.delete('v1/get/:id', deletePackageController.handle.bind(deletePackageController));
-packageRoutes.post('/v1/calculate-cost',calculateCostPackageController.run.bind(calculateCostPackageController))
+packageRoutes.patch('/v1/patch/updateStatus', updateStatusController.run.bind(updateStatusController));
+packageRoutes.delete('/v1/delete/:id', deletePackageController.handle.bind(deletePackageController));
+packageRoutes.post('/v1/post/calculate-cost',calculateCostPackageController.run.bind(calculateCostPackageController))
